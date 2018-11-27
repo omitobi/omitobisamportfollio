175e3ce56562bbf93872078ef1832ec1081ef653
Oluwatobi Samue Omisakin <omisakin.oluwatobi@gmail.com>
2018-11-27 22:30:57 +0200

    Add vue-beautiful-chat

--- src/components/Assistant.vue
+++ src/components/Assistant.vue
@@ -1,35 +1,29 @@
 <template>
-  <div id="app">
-    <div class="profile-card">
-      <profile-cover>
-      </profile-cover>
-      <router-view/>
-      <beautiful-chat
-        :participants="participants"
-        :titleImageUrl="titleImageUrl"
-        :onMessageWasSent="onMessageWasSent"
-        :messageList="messageList"
-        :newMessagesCount="newMessagesCount"
-        :isOpen="isChatOpen"
-        :close="closeChat"
-        :open="openChat"
-        :showEmoji="true"
-        :showFile="true"
-        :showTypingIndicator="showTypingIndicator"
-        :colors="colors"
-        :alwaysScrollToBottom="alwaysScrollToBottom"
-        :messageStyling="messageStyling" />
-    </div>
+  <div>
+    <beautiful-chat
+      :participants="participants"
+      :titleImageUrl="titleImageUrl"
+      :onMessageWasSent="onMessageWasSent"
+      :messageList="messageList"
+      :newMessagesCount="newMessagesCount"
+      :isOpen="isChatOpen"
+      :close="closeChat"
+      :open="openChat"
+      :showEmoji="true"
+      :showFile="true"
+      :showTypingIndicator="showTypingIndicator"
+      :colors="colors"
+      :alwaysScrollToBottom="alwaysScrollToBottom"
+      :messageStyling="messageStyling" />
   </div>
 </template>
 
 <script>
-import ProfileCover from './components/ProfileCover'
+import Chat from 'vue-beautiful-chat'
 
 export default {
-  components: {
-    ProfileCover},
-  name: 'App',
+  name: 'Assistant',
+  components: {Chat},
   data () {
     return {
       participants: [
@@ -104,25 +98,6 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 1%;
-  height: 80%;
-}
+<style scoped>
 
-.profile-card {
-  height: 100%;
-  border: 1px solid lightgray;
-  border-radius: 12px;
-  margin: 0 10% 0 10%;
-  /*box-shadow: inset 0 1px 0 rgba(255,255,255,.2), 0 1px 2px rgba(0,0,0,.05);*/
-  -webkit-box-shadow: 5px 5px 10px -5px rgba(0,0,0,0.5);
-  -moz-box-shadow: 5px 5px 10px -5px rgba(0,0,0,0.5);
-  box-shadow: 5px 5px 10px -5px rgba(0,0,0,0.5);
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Chat from 'vue-beautiful-chat'
 
 Vue.config.productionTip = false
+
+Vue.use(Chat, {})
 
 /* eslint-disable no-new */
 new Vue({
